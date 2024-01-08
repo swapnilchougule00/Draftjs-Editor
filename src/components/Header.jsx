89718588f5661e777160e36549c15b8ffe319a25
swapnilchougule7 <swapnilchougule00@gmail.com>
2024-01-08 23:32:36 +0530

    Added shortcuts keyBindings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import SaveButton from "./SaveButton";
 
-const Header = ({ handleSave }) => {
+const Header = () => {
   return (
-    <div className="flex justify-center w-full  items-center h-[8%] font-relway">
-      <h1 className="font-black text-center w-[60%] tracking-wider text-base">Demo Editor By Swapnil Chougule</h1>
-      <div className="flex  justify-self-end">
-        <SaveButton handleSave={handleSave} />
-        
-      </div>
+    <div className="flex justify-center w-[60%] items-center  font-relway">
+      <h1 className="font-black text-center  tracking-wider text-base">Demo Editor By Swapnil Chougule</h1>
     </div>
   );
 };
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,16 +1,71 @@
-import React, {  useState } from "react";
-import { Editor, EditorState, convertToRaw, convertFromRaw } from "draft-js";
+import React, { useEffect, useState } from "react";
+import {
+  Editor,
+  EditorState,
+  Modifier,
+  RichUtils,
+  convertFromRaw,
+  convertToRaw,
+  getDefaultKeyBinding,
+} from "draft-js";
 import "draft-js/dist/Draft.css";
 import Header from "./Header";
+import SaveButton from "./SaveButton";
+
+const styleMap = {
+  CODE: {
+    backgroundColor: "rgba(0, 0, 0, 0.05)",
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2,
+  },
+  HEADING_ONE: {
+    fontSize: "30px",
+    fontWeight: "700",
+  },
+  RED: {
+    color: "rgb(255, 0, 0)",
+  },
+};
+
+
+
+const  getModifiedText = (newState, offset = 1)=> {
+  const currentContent = newState.getCurrentContent();
+  const currentSelection = newState.getSelection();
+  const endOffset = currentSelection.getEndOffset();
+
+  const newContent = Modifier.replaceText(
+    currentContent,
+    currentSelection.merge({
+      anchorOffset: endOffset - offset,
+      focusOffset: endOffset,
+    }),
+    ""
+  );
+
+  return EditorState.push(newState, newContent, "remove-range");
+}
 
 const TextEditor = () => {
-
-  const savedData = localStorage.getItem("editorContent");
-  const state =  EditorState.createWithContent(
-    convertFromRaw(JSON.parse(savedData))
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
   );
-  const [editorState, setEditorState] = useState(state);
 
+ 
+
+  function handleEditorChange(newEditorState) {
+    setEditorState(newEditorState);
+  }
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("editorContent");
+    const state = savedData
+      ? EditorState.createWithContent(convertFromRaw(JSON.parse(savedData)))
+      : EditorState.createEmpty();
+
+    setEditorState(state);
+  }, []);
 
   const handleSave = (e) => {
     const contentState = editorState.getCurrentContent();
@@ -20,15 +75,89 @@ const TextEditor = () => {
     console.log(rawContent);
   };
 
+  const  handleKeyCommand = (command, newEditorState)=> {
+    let newState = RichUtils.handleKeyCommand(newEditorState, command);
+
+    if (command === "h1") {
+      newState = getModifiedText(newEditorState);
+      newState = RichUtils.toggleInlineStyle(newState, "HEADING_ONE");
+    }
+
+    if (command === "boldText") {
+      newState = getModifiedText(newEditorState);
+      newState = RichUtils.toggleInlineStyle(newState, "BOLD");
+    }
+
+    if (command === "redText") {
+      newState = getModifiedText(newEditorState, 2);
+      newState = RichUtils.toggleInlineStyle(newState, "RED");
+    }
+
+    if (command === "underline") {
+      newState = getModifiedText(newEditorState, 3);
+      newState = RichUtils.toggleInlineStyle(newState, "UNDERLINE");
+    }
+
+    if (newState) {
+      setEditorState(newState);
+      return "handled";
+    }
+    return "not-handled";
+  }
+
+  const  keyBindingFunction = (e)  =>{
+    if (
+      e.keyCode === 32 &&
+      editorState.getCurrentContent().getPlainText().endsWith("#") &&
+      editorState.getSelection().getStartOffset() === 1
+    ) {
+      return "h1";
+    }
+
+    if (
+      e.keyCode === 32 &&
+      editorState.getCurrentContent().getPlainText().endsWith("*") &&
+      editorState.getSelection().getStartOffset() === 1
+    ) {
+      return "boldText";
+    }
+
+    if (
+      e.keyCode === 32 &&
+      editorState.getCurrentContent().getPlainText().endsWith("**") &&
+      editorState.getSelection().getStartOffset() === 2
+    ) {
+      return "redText";
+    }
+
+    if (
+      e.keyCode === 32 &&
+      editorState.getCurrentContent().getPlainText().endsWith("***") &&
+      editorState.getSelection().getStartOffset() === 3
+    ) {
+      return "underline";
+    }
+
+    return getDefaultKeyBinding(e);
+  }
+
+
   return (
     <div className="w-full h-screen p-6 space-y-2">
-      <Header handleSave={handleSave} />
+      <div className="flex w-full h-[8%] justify-center">
+        <Header />
+        <SaveButton handleSave={handleSave} />
+      </div>
 
-      <div className="w-[80%] h-[90%] m-auto border-2 rounded-md border-black  p-2">
+      <div className="w-[80%] h-[90%] font-sans m-auto border-2 rounded-md border-black  p-2">
         <Editor
-          placeholder="type text here..."
-          onChange={setEditorState}
+          customStyleMap={styleMap}
           editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          keyBindingFn={keyBindingFunction}
+          onChange={handleEditorChange}
+          placeholder="type text here..."
+          spellCheck={true}
         />
       </div>
     </div>
